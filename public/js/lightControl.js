d4c0a26627ff038c207f0691d5dd7c5f1537c5db
Patrícia Cavoto <pat@macbook.lan>
2020-03-28 11:11:19 -0300

    Alterando serviço para post

--- public/js/lightControl.js
+++ public/js/lightControl.js
@@ -2,7 +2,7 @@ function control() {
     // se a lampada estava apagada, chama o serviço para acender
     // e muda a imagem para a lâmpada acesa
     if ($("#lightImage").attr("src").match("off")) {
-        $.get("/light/on", function (dataReturned, statusReturned) {
+        $.post("/light/on", function (dataReturned, statusReturned) {
             if (dataReturned.status == "OK")
                 $("#lightImage").attr("src", "/img/on.gif");
             alert("Data: " + dataReturned.data);
@@ -11,7 +11,7 @@ function control() {
     // se a lampada estava acesa, chama o serviço para apagar
     // e muda a imagem para a lâmpada apagada
     else {
-        $.get("/light/off", function (dataReturned, statusReturned) {
+        $.post("/light/off", function (dataReturned, statusReturned) {
             if (dataReturned.status == "OK")
                 $("#lightImage").attr("src", "/img/off.gif");
             alert("Data: " + dataReturned.data);
